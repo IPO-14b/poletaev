3f9a9405312b011c1ff1f48c11734e43a91a89a9
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-04-13 23:42:02 +0300

    Добавлен компонент AutoSizeInput, использующийся для создания текстового поля переменной ширины.

---
+++ src/auto-size-input.js
@@ -0,0 +1,33 @@
+let React = require("react");
+
+let MIN_WIDTH = 15;
+
+class AutoSizeInput extends React.PureComponent{
+
+	constructor(props){
+		super();
+		this.state = {
+			width: this._getWidth(props.value)
+		}
+	}
+
+	render(){
+		return <input type="text" className="auto-size" value={this.props.value} onChange={(e) => {
+			this.props.onChange(e); 
+			this.setState({width: this._getWidth(e.target.value)})
+		}} style={{width: this.state.width + "px"}} />
+	}
+
+	_getWidth(text){
+		let temp = document.createElement("span");
+		temp.innerHTML = text;
+		temp.classList.add("text-input");
+		document.body.appendChild(temp);
+		let width = temp.getBoundingClientRect().width;
+		document.body.removeChild(temp);
+		return Math.max(width - 6, MIN_WIDTH);
+	}
+
+}
+
+module.exports = AutoSizeInput;
