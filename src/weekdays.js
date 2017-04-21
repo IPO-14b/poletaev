07b50d1783a2158f082abe8d7fb281790dc8b771
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-04-21 17:57:51 +0300

    Измеенен стиль отступов для некоторых файлов

--- src/weekdays.js
+++ src/weekdays.js
@@ -1,19 +1,19 @@
  class WeekDay{
-	constructor(number, name, isHoliday){
-		this.number = number
-		this.name = name
-		this.isHoliday = typeof(isHoliday) !== undefined && isHoliday
-	}
+    constructor(number, name, isHoliday){
+        this.number = number
+        this.name = name
+        this.isHoliday = typeof(isHoliday) !== undefined && isHoliday
+    }
 }
 
 let weekDays = [
-	new WeekDay(0, "Понедельник"),
-	new WeekDay(1, "Вторник"),
-	new WeekDay(2, "Среда"),
-	new WeekDay(3, "Четверг"),
-	new WeekDay(4, "Пятница"),
-	new WeekDay(5, "Суббота", true),
-	new WeekDay(6, "Воскресенье", true)
+    new WeekDay(0, "Понедельник"),
+    new WeekDay(1, "Вторник"),
+    new WeekDay(2, "Среда"),
+    new WeekDay(3, "Четверг"),
+    new WeekDay(4, "Пятница"),
+    new WeekDay(5, "Суббота", true),
+    new WeekDay(6, "Воскресенье", true)
 ]
 
 module.exports = weekDays;
