ada41d17c4c14a6c95e8d7281e7aa329a6518abb
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-04-22 13:06:18 +0300

    Добавлены элементы UI для редактирования параметров генерации файла календаря

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,9 +1,32 @@
 let React = require("react");
 
 class ToolBar extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+            startDate: "2017-03-12",
+            endDate: "2017-05-16"
+        };
+	}
+
     render(){
         return <div className="toolbar">
             <div className="logo"></div>
+            <div className="generation">
+            	<label>
+            		<span className="label">Начало семестра:</span>
+                    <input type="date" value={this.state.startDate} max={this.state.endDate} 
+                        onChange={e => this.setState({startDate: e.target.value})}/>
+            	</label>
+
+                <label>
+                    <span className="label">Конец семестра:</span>
+                    <input type="date" value={this.state.endDate} min={this.state.startDate} 
+                        onChange={e => this.setState({startDate: e.target.value})}/>
+                </label>
+
+                <input type="button" className="button" value="Сгенерировать ical" />
+            </div>
         </div>
     }
 }
